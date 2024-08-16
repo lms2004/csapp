7779a6a4ec0688e9eaf937f130dd15dbd6e6fa93
刘铭澍 <mxylms1210@163.com>
2024-08-17 01:37:53 +0800

    开始完成bomb实验的Phase3

--- labs/bomb_/bomb/Phases.cpp
+++ labs/bomb_/bomb/Phases.cpp
@@ -31,3 +31,56 @@ int main(){
 
     return 0;
 }
+
+//  phase_3
+int main(char * output){
+    int x,y;
+    int  check = sscanf(output,"%d %d",&x,&y); // 四个元素
+
+    if (check <= 1){
+         explode_bomb();
+    }
+
+    if(x > 7){
+        explode_bomb();
+    }
+
+    int ret;
+    switch (x) {
+        case 0:
+            ret = 0xcf; // corresponding to 0x400f7c
+            break;
+        case 1:
+            ret = 0x2c3; // corresponding to 0x400f83
+            break;
+        case 2:
+            ret = 0x100; // corresponding to 0x400f8a
+            break;
+        case 3:
+            ret = 0x185; // corresponding to 0x400f91
+            break;
+        case 4:
+            ret = 0xce; // corresponding to 0x400f98
+            break;
+        case 5:
+            ret = 0x2aa; // corresponding to 0x400f9f
+            break;
+        case 6:
+            ret = 0x147; // corresponding to 0x400fa6
+            break;
+        case 7:
+            explode_bomb(); // corresponding to 0x400fad
+            break;
+        case 8:
+            ret = 0x0; // corresponding to 0x400fb2
+            break;
+        case 9:
+            ret = 0x137; // corresponding to 0x400fb9
+            break;
+    }
+    if (ret != y){
+        explode_bomb();
+    }
+
+    return 0;
+}
